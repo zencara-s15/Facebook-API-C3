2411e8389befdc21a6daf0bf5cf97e305a9f0473
Sereyputhireach.chin <sereyputhireach.chin@student.passerellesnumeriques.org>
2024-06-12 08:37:53 +0700

    User can register with validation

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Auth;
 
-use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
 use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -22,52 +23,51 @@ class AuthController extends Controller
         $user_exist = User::where('email', $request->email)->first();
         if ($user_exist) {
             return response()->json([
-                'message'   => 'User already exist',
+                'message'   => 'User already exists',
                 'success'    => false,
             ]);
         }
 
         $password = Hash::make($request->password);
-
         $user = User::create([
             'name'      => $request->name,
             'email'     => $request->email,
             'password'  => $password,
         ]);
+
         return response()->json([
-            'message'   => 'New user register successfully',
+            'message'   => 'New user registered successfully',
             'success'    => true,
         ]);
-        
-
     }
 
     public function login(Request $request): JsonResponse
     {
         $validator = Validator::make($request->all(), [
-            'email'     => 'required|string|max:255',
+            'email'     => 'required|string|email|max:255',
             'password'  => 'required|string'
-          ]);
+        ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json($validator->errors(), 422);
         }
 
         $credentials = $request->only('email', 'password');
 
         if (!Auth::attempt($credentials)) {
             return response()->json([
-                'message' => 'User not found'
+                'message' => 'Incorrect username or password'
             ], 401);
         }
 
-        $user   = User::where('email', $request->email)->firstOrFail();
-        $token  = $user->createToken('auth_token')->plainTextToken;
+        $user = User::where('email', $request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
 
         return response()->json([
-            'message'       => 'Login success',
+            'message'       => 'Login successfully',
             'access_token'  => $token,
-            'token_type'    => 'Bearer'
+            'token_type'    => 'Bearer',
+            'user' => $user
         ]);
     }
 
@@ -77,8 +77,8 @@ class AuthController extends Controller
         $permissions = $user->getAllPermissions();
         $roles = $user->getRoleNames();
         return response()->json([
-            'message' => 'Login success',
-            'data' =>$user,
+            'message' => 'Login successful',
+            'data' => $user,
         ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
+// use App\Http\Controllers\Auth\AuthController;
+
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
